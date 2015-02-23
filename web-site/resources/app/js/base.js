1693808a7bb1b41dd631583747bddf7c56bdc6ae
romelgomez <bmxquiksilver7185@gmail.com>
2015-02-22 22:23:50 -0430

    some improves

--- web-site/resources/app/js/base.js
+++ web-site/resources/app/js/base.js
@@ -1,4 +1,60 @@
-angular.module("App",['angular-underscore/filters'])
+angular.module("Task",['angular-underscore/filters'])
+    .provider('tasks',function(){
+
+        var tasks = [
+            {task:"Aprender Angular",   status:false},
+            {task:"Aprender GoLang",    status:false},
+            {task:"Aprender Git",       status:false},
+            {task:"Aprender Yeoman",    status:true}
+        ];
+
+        return {
+            $get: function(){
+                return {
+                    'add':function(task){
+                        tasks.push({"task":task,"status":false});
+                    },
+                    'get': function () {
+                        return tasks;
+                    },
+                    'tasks':function(){
+                        return tasks.length;
+                    },
+                    'changeAllStatus' : function(all){
+                        if(all){
+                            angular.forEach(tasks, function (value, key) {
+                                tasks[key].status = true;
+                            });
+                        }else{
+                            angular.forEach(tasks, function (value, key) {
+                                tasks[key].status = false;
+                            });
+                        }
+                    },
+                    'left': function(){
+                        // _.filter -> http://underscorejs.org/#filter
+                        var completed  = _.filter(tasks,function(task){
+                            return task.status;
+                        });
+                        return tasks.length-completed.length;
+                    },
+                    'length':function(){
+                        return tasks.length;
+                    },
+                    'delete' : function(){
+                        // _.filter -> http://underscorejs.org/#filter
+                        tasks = _.filter(tasks,function(task){
+                            return !task.status;
+                        });
+                    }
+                }
+            }
+        }
+
+    });
+
+
+angular.module("App",['Task'])
     .controller("ImagesController",function($scope){
 
         $scope.images = [
@@ -20,56 +76,40 @@ angular.module("App",['angular-underscore/filters'])
         ];
 
     })
-    .controller("TaskController",['$scope','$log',function($scope,$log){
+    .controller("TasksController",['$scope','$log','tasks',function($scope,$log,tasks){
 
-        var tasks = {
-          defaultList: [
-            {text:"Aprender Angular",   status:false},
-            {text:"Aprender GoLang",    status:false},
-            {text:"Aprender Git",       status:false},
-            {text:"Aprender Yeoman",    status:true}
-          ]
-        };
-
-        $scope.checkOrUncheckTasks = function(all){
-            if(all){
-                angular.forEach(tasks.defaultList, function (value, key) {
-                    tasks.defaultList[key].status = true;
-                });
-            }else{
-                angular.forEach(tasks.defaultList, function (value, key) {
-                    tasks.defaultList[key].status = false;
-                });
+        $scope.tasks = {
+            'add':function(){
+                tasks.add($scope.tasks.task);
+                $scope.tasks.task = '';
+            },
+            'get':function(){
+                return tasks.get();
+            },
+            'left':function(){
+                return tasks.left()
+            },
+            'length':function(){
+                return tasks.length();
+            },
+            'delete':function(){
+                return tasks.delete()
+            },
+            'changeAllStatus':function(status){
+                tasks.changeAllStatus(status)
             }
         };
 
-        $scope.getDefaultTaskList = function(){
-            return tasks.defaultList;
-        };
 
-        $scope.addTask = function(){
-            tasks.defaultList.push({"text":$scope.task,"status":false});
-            $scope.task = '';
-        };
+        //var VALID_LENGTH = 3;
+        //$scope.hasValidLength = function(){
+        //    var validLength = false;
+        //
+        //    if ($scope.task.length > VALID_LENGTH) {
+        //        validLength = true
+        //    }
+        //
+        //    return validLength;
+        //};
 
-        $scope.taskListTotal = function(){
-            return tasks.defaultList.length;
-        };
-
-        $scope.taskListLeft = function(){
-            // _.filter -> http://underscorejs.org/#filter
-            var completed  = _.filter(tasks.defaultList,function(task){
-                return task.status;
-            });
-            return tasks.defaultList.length-completed.length;
-        };
-
-        $scope.deleteTask = function(){
-            // _.filter -> http://underscorejs.org/#filter
-            tasks.defaultList = _.filter(tasks.defaultList,function(task){
-                return !task.status;
-            });
-        };
-
-    }
-    ]);
+    }]);
