8e7599dd0764d8fd1ee9b9766c408afb84cb738d
romelgomez <bmxquiksilver7185@gmail.com>
2015-03-20 23:07:06 -0430

    some changes

--- client/app/assets/scripts/app.js
+++ client/app/assets/scripts/app.js
@@ -129,6 +129,10 @@ angular.module('routes',['ui.router','Task'])
                 url:'/to-do-example',
                 templateUrl:'assets/partials/examples/to-do-example.html',
                 controller:'TasksController'
+            })
+            .state('maximizing-angular-directives',{
+                url:'/maximizing-angular-directives',
+                templateUrl:'assets/partials/angular-web-application-development-cookbook/maximizing-angular-directives.html',
             });
 
     }])
@@ -154,80 +158,167 @@ angular.module('routes',['ui.router','Task'])
 
     }]);
 
-angular.module('directives',[])
-    .directive('myDirective',[function(){
-        return {
-            restrict:'E',
-            templateUrl: 'my-template.html'
-        };
-    }])
-    .directive('elementDirective',['$log',function($log){
-        return {
-            restrict:'E',
-            template: '<div class="alert alert-info" > Element directive with replace property undefined</div>',
-            link:function(scope,el,attrs){
-              $log.log('*** Element directive with replace property undefined ***');
-              $log.log(el.html());
-              $log.log(attrs.someAttr);
+angular.module('buildingSimpleElementDirective',[])
+  .directive('myDirective',[function(){
+    return {
+      restrict:'E',
+      templateUrl: 'my-template.html'
+    };
+  }]);
+
+angular.module('workingThroughTheDirectiveSpectrum',[])
+  .directive('elementDirective',['$log',function($log){
+    return {
+      restrict:'E',
+      template: '<div class="alert alert-info" > Element directive with replace property undefined</div>',
+      link:function(scope,el,attrs){
+        $log.log('*** Element directive with replace property undefined ***');
+        $log.log(el.html());
+        $log.log(attrs.someAttr);
+      }
+    };
+  }])
+  .directive('elementDirectiveRemplace',['$log',function($log){
+    return {
+      restrict:'E',
+      replace:true,
+      template: '<div class="alert alert-info" > Element directive with replace property set to true</div>',
+      link:function(scope,el,attrs){
+        $log.log('*** Element directive with replace property set to true ***');
+        $log.log(el.html());
+        $log.log(attrs.someAttr);
+      }
+    };
+  }])
+  .directive('attributeDirective',['$log',function($log){
+    return {
+      restrict:'A',
+      template: '<div class="alert alert-info" style="margin-bottom: 0;" > The Attribute directive template property content.</div>',
+      link:function(scope, el, attrs){
+        $log.log('*** The Attribute directive ***');
+        $log.log(el.html());
+        $log.log(attrs.someAttr);
+        $log.log(attrs.attributeDirective);
+      }
+    };
+
+  }])
+  .directive('classDirective',['$log',function($log){
+    return {
+      restrict:'C',
+      template: '<div class="alert alert-info" style="margin-bottom: 0;" > The class directive template.</div>',
+      link:function(scope, el, attrs){
+        $log.log('*** The class directive ***');
+        $log.log(el.html());
+        $log.log(el.hasClass('normal-class'));
+        $log.log(attrs.someAttr);
+        $log.log(attrs.classDirective);
+      }
+    };
+  }]);
+
+angular.module('manipulatingTheDom',[])
+  .directive('counter',['$log',function($log){
+    return {
+      restrict:'A',
+      link:function(scope, el, attrs){
+        $log.log('*** The counter directive ***');
+        var incr = parseInt(attrs.incr || 1), val = 0;
+        el.bind('click',function(){
+          el.html(val+=incr);
+        });
+
+        $log.log(el.html());
+      }
+    };
+
+  }]);
+
+angular.module('likingDirectives',[])
+  .directive('vectorText',['$log','$document',function($log,$document){
+    return {
+      restrict:'A',
+      template:'<span>{{ heading }}</span>',
+      link:function(scope, el, attrs){
+        $log.log('*** The Linking directives ***');
+
+        el.css({
+          'float':'left',
+          'padding': attrs.buffer+'px',
+          'background-color':'antiquewhite'
+        });
+
+        scope.heading = '';
+
+        $document.on('mousemove',function(event){
+          scope.$apply(function(){
+
+            if(event.pageY < 300){
+              scope.heading = 'N';
+            }else{
+              scope.heading = 'S';
             }
-        };
-    }])
-    .directive('elementDirectiveRemplace',['$log',function($log){
-        return {
-            restrict:'E',
-            replace:true,
-            template: '<div class="alert alert-info" > Element directive with replace property set to true</div>',
-            link:function(scope,el,attrs){
-              $log.log('*** Element directive with replace property set to true ***');
-              $log.log(el.html());
-              $log.log(attrs.someAttr);
+
+            if(event.pageX < 300){
+              scope.heading += 'W';
+            }else{
+              scope.heading += 'E';
             }
-        };
-    }])
-    .directive('attributeDirective',['$log',function($log){
-        return {
-          restrict:'A',
-          template: '<div class="alert alert-info" style="margin-bottom: 0;" > The Attribute directive template property content.</div>',
-          link:function(scope, el, attrs){
-              $log.log('*** The Attribute directive ***');
-              $log.log(el.html());
-              $log.log(attrs.someAttr);
-              $log.log(attrs.attributeDirective);
-          }
-        };
 
-    }])
-    .directive('classDirective',['$log',function($log){
-        return {
-          restrict:'C',
-          template: '<div class="alert alert-info" style="margin-bottom: 0;" > The class directive template.</div>',
-          link:function(scope, el, attrs){
-              $log.log('*** The class directive ***');
-              $log.log(el.html());
-              $log.log(el.hasClass('normal-class'));
-              $log.log(attrs.someAttr);
-              $log.log(attrs.classDirective);
-          }
-        };
+          });
+        });
 
-    }])
-    .directive('counter',['$log',function($log){
-        return {
-          restrict:'A',
-          link:function(scope, el, attrs){
-            $log.log('*** The counter directive ***');
-            var incr = parseInt(attrs.incr || 1), val = 0;
-            el.bind('click',function(){
-                el.html(val+=incr);
-            });
+        $log.log(el.html());
 
-            $log.log(el.html());
-          }
-        };
+      }
+    };
 
-    }]);
+  }]);
+
+angular.module('interfacingDirectiveUsingIsolateScope',[])
+  .controller('IsolateScopeController',['$scope','$log',function($scope,$log){
+    $scope.outerval = 'myData';
+    $scope.func = function(){
+      $log.log('invoked');
+    };
+  }])
+  .directive('iso',[function(){
+    return {};
+  }]);
+
+angular.module('interactionBetweenNestedDirectives',[]);
+
+angular.module('optionalNestedDirectiveControllers',[]);
+
+angular.module('directiveScopeInheritance',[]);
+
+angular.module('directiveTemplating',[]);
+
+angular.module('isolateScope',[]);
+
+angular.module('directiveTransclusion',[]);
+
+angular.module('recursiveDirectives',[]);
+
+angular.module('maximizingAngularDirectives',
+  [
+    'buildingSimpleElementDirective',
+    'workingThroughTheDirectiveSpectrum',
+    'manipulatingTheDom',
+    'likingDirectives',
+    'interfacingDirectiveUsingIsolateScope',
+    'interactionBetweenNestedDirectives',
+    'optionalNestedDirectiveControllers',
+    'directiveScopeInheritance',
+    'directiveTemplating',
+    'isolateScope',
+    'directiveTransclusion',
+    'recursiveDirectives'
+  ]);
+
+angular.module('angular-web-application-development-cookbook',['maximizingAngularDirectives']);
 
 
-angular.module('app',['routes','directives']);
+angular.module('app',['routes','angular-web-application-development-cookbook']);
 
 //prettyPrint();
