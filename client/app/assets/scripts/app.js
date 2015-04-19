6b38f9b1706b5089b36863818fffd22ab2c3df70
romelgomez <bmxquiksilver7185@gmail.com>
2015-04-19 07:57:25 -0430

    some changes

--- client/app/assets/scripts/app.js
+++ client/app/assets/scripts/app.js
@@ -132,32 +132,13 @@ angular.module('routes',['ui.router'])
                 templateUrl:'assets/partials/contents/directives.html'
             });
 
-        $stateProvider
-            .state('ngRepeatExample',{
-                url:'/ng-repeat-example',
-                templateUrl:'assets/partials/directives/ng-repeat-example.html',
-                controller:'ngRepeatExample'
-            })
-            .state('toDoExample',{
-                url:'/to-do-example',
-                templateUrl:'assets/partials/examples/to-do-example.html',
-                controller:'TasksController'
-            })
-            .state('maximizing-angular-directives',{
-                url:'/maximizing-angular-directives',
-                templateUrl:'assets/partials/angular-web-application-development-cookbook/maximizing-angular-directives.html'
-            })
-            .state('gettingStarted',{
-                url:'/getting-started',
-                templateUrl:'assets/partials/getting-started/getting-started.html'
-            });
 
     }]);
 
 // Restangular
 
 angular.module('forms',['ngMessages','restangular','uuid'])
-    .factory('UsersService',['Restangular','$log',function(Restangular,$log){
+    .factory('UsersService',['Restangular','$log',function(Restangular){
 
         var users = Restangular.all('users');
 
@@ -195,7 +176,7 @@ angular.module('forms',['ngMessages','restangular','uuid'])
             }
         };
     }])
-    .controller('UsersController',['$scope','$log','UserMockService',function($scope,$log,UserMockService){
+    .controller('UsersController',['$scope','$log','UserMockService','Restangular',function($scope,$log,UserMockService,Restangular){
 
         var original = angular.copy($scope.user = {
             name: '',
@@ -221,6 +202,9 @@ angular.module('forms',['ngMessages','restangular','uuid'])
             $scope.userForm.$setPristine();
         };
 
+        $scope.testRest = function(){
+          Restangular.allUrl('google','http://www.google.com').getList();
+        };
 
     }]);
 
@@ -257,269 +241,3 @@ angular.module('directives',[])
 
 
 angular.module('app',['routes','gist','directives','forms','task']);
-
-
-
-
-//
-//angular.module('buildingSimpleElementDirective',[])
-//  .directive('myDirective',[function(){
-//    return {
-//      restrict:'E',
-//      templateUrl: 'my-template.html'
-//    };
-//  }]);
-//
-//angular.module('workingThroughTheDirectiveSpectrum',[])
-//  .directive('elementDirective',['$log',function(){
-//    return {
-//      restrict:'E',
-//      template: '<div class="alert alert-info" > Element directive with replace property undefined</div>',
-//      link:function(scope,el,attrs){
-////        $log.log('*** Element directive with replace property undefined ***');
-////        $log.log(el.html());
-////        $log.log(attrs.someAttr);
-//      }
-//    };
-//  }])
-//  .directive('elementDirectiveRemplace',['$log',function(){
-//    return {
-//      restrict:'E',
-//      replace:true,
-//      template: '<div class="alert alert-info" > Element directive with replace property set to true</div>',
-//      link:function(scope,el,attrs){
-////        $log.log('*** Element directive with replace property set to true ***');
-////        $log.log(el.html());
-////        $log.log(attrs.someAttr);
-//      }
-//    };
-//  }])
-//  .directive('attributeDirective',['$log',function(){
-//    return {
-//      restrict:'A',
-//      template: '<div class="alert alert-info" style="margin-bottom: 0;" > The Attribute directive template property content.</div>',
-//      link:function(scope, el, attrs){
-////        $log.log('*** The Attribute directive ***');
-////        $log.log(el.html());
-////        $log.log(attrs.someAttr);
-////        $log.log(attrs.attributeDirective);
-//      }
-//    };
-//
-//  }])
-//  .directive('classDirective',['$log',function(){
-//    return {
-//      restrict:'C',
-//      template: '<div class="alert alert-info" style="margin-bottom: 0;" > The class directive template.</div>',
-//      link:function(scope, el, attrs){
-////        $log.log('*** The class directive ***');
-////        $log.log(el.html());
-////        $log.log(el.hasClass('normal-class'));
-////        $log.log(attrs.someAttr);
-////        $log.log(attrs.classDirective);
-//      }
-//    };
-//  }]);
-//
-//angular.module('manipulatingTheDom',[])
-//  .directive('counter',['$log',function(){
-//    return {
-//      restrict:'A',
-//      link:function(scope, el, attrs){
-////        $log.log('*** The counter directive ***');
-//        var incr = parseInt(attrs.incr || 1), val = 0;
-//        el.bind('click',function(){
-//          el.html(val+=incr);
-//        });
-//
-////        $log.log(el.html());
-//      }
-//    };
-//
-//  }]);
-//
-//angular.module('likingDirectives',[])
-//  .directive('vectorText',['$log','$document',function($log,$document){
-//    return {
-//      restrict:'A',
-//      template:'<span>{{ heading }}</span>',
-//      link:function(scope, el, attrs){
-////        $log.log('*** The Linking directives ***');
-//
-//        el.css({
-//          'float':'left',
-//          'padding': attrs.buffer+'px',
-//          'background-color':'antiquewhite'
-//        });
-//
-//        scope.heading = '';
-//
-//        $document.on('mousemove',function(event){
-//          scope.$apply(function(){
-//
-//            if(event.pageY < 300){
-//              scope.heading = 'N';
-//            }else{
-//              scope.heading = 'S';
-//            }
-//
-//            if(event.pageX < 300){
-//              scope.heading += 'W';
-//            }else{
-//              scope.heading += 'E';
-//            }
-//
-//          });
-//        });
-//
-////        $log.log(el.html());
-//
-//      }
-//    };
-//
-//  }]);
-//
-//angular.module('interfacingDirectiveUsingIsolateScope',[])
-//  .controller('IsolateScopeController',['$scope','$log',function($scope){
-//    $scope.controllerValue = 'myData';
-//    $scope.controllerMethod = function (){
-////      $log.log('This is controller method called front iso3 directive');
-//    };
-//
-//  }])
-//  .directive('iso',['$log',function(){
-//    return {
-//      scope:{
-//        'readOnlyIsolateScopeValue':'@attrValue'
-//      },
-//      link:function(scope){
-////        $log.log('*** Interfacing Directive Using Isolate Scope ***');
-//
-//
-////        $log.log('readOnlyIsolateScopeValue:', scope.readOnlyIsolateScopeValue);
-//
-//      }
-//    };
-//  }])
-//  .directive('iso2',['$log',function(){
-//    return {
-//      scope:{
-//        'bindingFrontTheParentScope':'=attrValue'
-//      },
-//      link:function(scope){
-////        $log.log('*** Interfacing Directive Using Isolate Scope ***');
-////
-////
-////        $log.log(scope.bindingFrontTheParentScope);
-//
-//      }
-//    };
-//  }])
-//  .directive('iso3',['$log',function(){
-//    return {
-//      scope:{
-//        'methodFrontTheParentScope':'&attrValue'
-//      },
-//      link:function(scope){
-////        $log.log('*** Interfacing Directive Using Isolate Scope ***');
-//
-//        scope.methodFrontTheParentScope();
-//      }
-//    };
-//  }]);
-//
-//angular.module('interactionBetweenNestedDirectives',[])
-//  .directive('parentDirective',['$log',function(){
-//    return {
-//      controller: function(){
-//        this.identify = function(){
-////          $log.log('Parent!');
-//        };
-//      }
-//    };
-//  }])
-//  .directive('siblingDirective',['$log',function(){
-//    return {
-//      controller: function(){
-//        this.identify = function(){
-////          $log.log('sibling!');
-//        };
-//      }
-//    };
-//  }])
-//  .directive('childDirective',['$log',function(){
-//    return {
-//      require: ['^parentDirective','^siblingDirective'],
-//      link:function(scope,el,attrs,ctrls){
-////        $log.log('*** Interaction Between Nested Directives ***');
-//
-//
-//        ctrls[0].identify();
-//        ctrls[1].identify();
-//      }
-//    };
-//  }]);
-//
-//angular.module('optionalNestedDirectiveControllers',[])
-//  .directive('parentDirective2',['$log',function(){
-//    return {
-//      controller: function(){
-//        this.identify = function(){
-////          $log.log('Parent!');
-//        };
-//      }
-//    };
-//  }])
-//  .directive('siblingDirective2',['$log',function(){
-//    return {
-//      controller: function(){
-//        this.identify = function(){
-////          $log.log('sibling!');
-//        };
-//      }
-//    };
-//  }])
-//  .directive('childDirective2',['$log',function(){
-//    return {
-//      require: ['^parentDirective2','^siblingDirective2','^?missingDirective'],
-//      link:function(scope,el,attrs,ctrls){
-////        $log.log('*** Optional Nested Directive Controllers ***');
-//
-//        ctrls[0].identify();
-//        ctrls[1].identify();
-////        $log.log(ctrls[2]);
-//
-//      }
-//    };
-//  }]);
-//
-//angular.module('directiveScopeInheritance',[]);
-//
-//angular.module('directiveTemplating',[]);
-//
-//angular.module('isolateScope',[]);
-//
-//angular.module('directiveTransclusion',[]);
-//
-//angular.module('recursiveDirectives',[]);
-//
-//angular.module('maximizingAngularDirectives',
-//  [
-//    'buildingSimpleElementDirective',
-//    'workingThroughTheDirectiveSpectrum',
-//    'manipulatingTheDom',
-//    'likingDirectives',
-//    'interfacingDirectiveUsingIsolateScope',
-//    'interactionBetweenNestedDirectives',
-//    'optionalNestedDirectiveControllers',
-//    'directiveScopeInheritance',
-//    'directiveTemplating',
-//    'isolateScope',
-//    'directiveTransclusion',
-//    'recursiveDirectives'
-//  ]);
-//
-//angular.module('angular-web-application-development-cookbook',['maximizingAngularDirectives']);
-
-
-//prettyPrint();
